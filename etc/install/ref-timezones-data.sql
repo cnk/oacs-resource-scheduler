08e008ba32681d77835532a9104cc3e378d5fc57
Cynthia Kiser <cnk@caltech.edu>
2009-01-19 16:28:46 -0800

    removing OACS stuff - to eventually leave only ctrl packages

--- etc/install/ref-timezones-data.sql
+++
@@ -1,8 +0,0 @@
--- packages/ref-timezones/sql/oracle/ref-timezone-data.sql
---
--- Part of the timezone reference data
--- 
--- @cvs-id $Id: ref-timezones-data.sql,v 1.1 2003-09-04 16:51:26 joela Exp $
-
-insert into timezones values (1,'Africa/Abidjan','+000000');     
-insert into timezones values (2,'Africa/Accra','+000000');       
